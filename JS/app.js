1e126054eadce99c000f9946a3b1ea2987544893
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-07-14 17:14:37 +0530

    login & signup page

--- JS/app.js
+++ JS/app.js
@@ -18,23 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
             bookBtn.classList.remove("active");
         }
     });
+    // Login & Sign Up
+    const loginBtn = document.querySelector('.login-btn');
+    const signupBtn = document.querySelector('.signup-btn');
+
+    loginBtn.addEventListener('click', ()=> {
+      window.location.href = 'login.html'
+    });
+
+    signupBtn.addEventListener('click', ()=> {
+      window.location.href = "sign-up.html"
+    })
+
   // Modal Toggle
   const modals = document.querySelectorAll(".modal");
-  const openButtons = document.querySelectorAll(
-    ".login-btn, .signup-btn"
-  );
   const closeButtons = document.querySelectorAll(".close-btn");
 
-  openButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const modalId =
-        button
-          .getAttribute("onclick")
-          ?.match(/open(\w+)Modal/)?.[1]
-          .toLowerCase() || "auth";
-      document.getElementById(`${modalId}-modal`).style.display = "block";
-    });
-  });
 
   closeButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
