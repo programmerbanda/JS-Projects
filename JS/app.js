96d48b1a995949eb2775b8556e05109d08e7743e
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-07-21 18:38:19 +0530

    update js

--- JS/app.js
+++ JS/app.js
@@ -1,34 +1,3 @@
-document.querySelectorAll('.scroll-track').forEach(container => {
-  let isDown = false;
-  let startX;
-  let scrollLeft;
-
-  container.addEventListener('mousedown', e => {
-    isDown = true;
-    container.classList.add('active');
-    startX = e.pageX - container.offsetLeft;
-    scrollLeft = container.scrollLeft;
-  });
-
-  container.addEventListener('mouseleave', () => {
-    isDown = false;
-    container.classList.remove('active');
-  });
-
-  container.addEventListener('mouseup', () => {
-    isDown = false;
-    container.classList.remove('active');
-  });
-
-  container.addEventListener('mousemove', e => {
-    if(!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - container.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    container.scrollLeft = scrollLeft - walk;
-  });
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   // hamburger
 const hamburger = document.querySelector(".hamburger");
